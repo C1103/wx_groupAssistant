a59b06f384df8a1f2552c63457f06180ff18d5ae
C1103 <1103384457@qq.com>
2017-12-03 00:12:33 +0800

    修改template结构 完成js部分 及数据渲染

--- pages/template/template.js
+++ pages/template/template.js
@@ -1,3 +1,16 @@
 Page({
-
+    data: {
+        pictures: []
+    },
+    goToActivePage: function(e) {
+        var index = e.target.setdata.current;
+        wx.navigateTo({
+            url: '../add_active/add_active?id=index',
+        })
+    },
+    onLoad: function() {
+        wx.setNavigationBarTitle({
+            title: '选择模板'
+        })
+    }
 })
