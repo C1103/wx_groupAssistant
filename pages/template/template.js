3b3ed30857cd45e0e5f383f876a069cceec71456
C1103 <1103384457@qq.com>
2017-12-03 13:43:27 +0800

    完成template与add_active即活动页面与图片模板页面交互

--- pages/template/template.js
+++ pages/template/template.js
@@ -2,15 +2,31 @@ Page({
     data: {
         pictures: []
     },
-    goToActivePage: function(e) {
-        var index = e.target.setdata.current;
-        wx.navigateTo({
-            url: '../add_active/add_active?id=index',
-        })
-    },
+    // goToActivePage: function(e) {
+    //     var index = e.target.dataset.current;
+    //     console.log(e.target.dataset.current);
+    //     // wx.navigateTo({
+    //     //     url: "../add_active/add_active?id='index'",
+    //     // })
+    // },
     onLoad: function() {
+        var that = this;
         wx.setNavigationBarTitle({
             title: '选择模板'
         })
+        wx.request({
+            url: 'http://www.easy-mock.com/mock/5a1e246e365fb14a83207162/first_time/template',
+            data: {},
+            header: {
+                'content-type': 'application.json'
+            },
+            success: function(res) {
+                console.log(res.data.data.data);
+                that.setData({
+                        pictures: res.data.data.data,
+                    })
+                    // console.log(that.data.timeData_one);
+            }
+        });
     }
 })
